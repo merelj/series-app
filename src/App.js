77d994f3559572ca70efae59b58cd0a930970b67
Inessa Luerce <luerce.id@gmail.com>
2018-07-16 18:58:46 -0300

    refactor: App now is stateless

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import Series from './containers/Series';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">TV Series List</h1>
-        </header>
-        <Series />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">TV Series List</h1>
+      </header>
+      <Series />
+    </div>
+  );
 }
 
 export default App;
