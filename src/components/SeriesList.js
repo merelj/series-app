564b70bcaaf02c93d774510c047aa3788f077a8b
Inessa Luerce <luerce.id@gmail.com>
2018-07-16 19:16:40 -0300

    refactor: unecessary comment removed

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './styles/SeriesList.css';
 import SeriesListItem from './SeriesListItem.js';
 
-// const SeriesListItem = ({series}) => (
-//   <li key={series.show.id}>
-//     {series.show.name}
-//   </li>
-// )
-
 const SeriesList = ({list}) => {
   return (
     <div>
