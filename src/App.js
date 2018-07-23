1ea77a7c6336cf627c89492cbf8eb1bc04ef8b49
inessa luerce <idluerce@thoughtworks.com>
2018-07-23 20:21:38 -0300

    Add new icon on page title, removed Intro information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Series from './containers/Series';
 import './App.css';
 import './assets/semantic-ui-css/semantic.css';
@@ -6,11 +6,20 @@ import './assets/semantic-ui-css/semantic.css';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">TV Series List</h1>
-      </header>
-      <Series />
+    <header className="App-header">
+      <h1 className="App-title">TV Series List</h1>
+    </header>
+    <Series/>
+
+
+    <div>Icons made by
+      <a href="http://www.freepik.com" title="Freepik"> Freepik </a>
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com </a>
+      is licensed by
+      <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"> CC 3.0 BY </a>
     </div>
+  </div>
   );
 }
 
