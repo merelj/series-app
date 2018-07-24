8490893c9738ecc3b6c7e95b4c8779ca0d1c6371
inessa luerce <idluerce@thoughtworks.com>
2018-07-24 20:37:58 -0300

    Add language to search results

--- src/components/SeriesListItem.js
+++ src/components/SeriesListItem.js
@@ -3,7 +3,12 @@ import './styles/SeriesList.css';
 
 const SeriesListItem = ({series}) => (
   <li className="four wide column" key={series.show.id}>
-    {series.show.name}
+    <div>
+      <ul>
+        <li>{series.show.name}</li>
+        <li>Language: {series.show.language}</li>
+      </ul>
+    </div>
   </li>
 )
 
