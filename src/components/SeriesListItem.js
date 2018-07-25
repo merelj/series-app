26f533990e13d9fa19cf51bccc7122f943a8e86e
inessa luerce <idluerce@thoughtworks.com>
2018-07-24 21:32:43 -0300

    Add images to search results

--- src/components/SeriesListItem.js
+++ src/components/SeriesListItem.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './styles/SeriesList.css';
 
-const SeriesListItem = ({series}) => (
-  <li className="four wide column" key={series.show.id}>
-    <div>
-      <ul>
-        <li>{series.show.name}</li>
-        <li>Language: {series.show.language}</li>
-      </ul>
-    </div>
-  </li>
-)
+
+const SeriesListItem = ({series}) => {
+
+  const imageUrl = series.show.image.medium;
+
+  return (
+    <li className="four wide column" key={series.show.id}>
+      <div>
+        <ul>
+          <li>{series.show.name}</li>
+          <li>Language: {series.show.language}</li>
+          <img src={imageUrl}></img>
+        </ul>
+      </div>
+    </li>
+  );
+
+}
 
 export default SeriesListItem;
