f30a0762fba8eaef453a7aa3b707eeb742df7942
inessa luerce <idluerce@thoughtworks.com>
2018-07-24 22:13:31 -0300

    Add type of serie to results

--- src/App.js
+++ src/App.js
@@ -6,19 +6,9 @@ import './assets/semantic-ui-css/semantic.css';
 const App = () => {
   return (
     <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">TV Series List</h1>
-    </header>
+      <h1 className="ui header">TV Series List</h1>
     <Series/>
 
-
-    <div>Icons made by
-      <a href="http://www.freepik.com" title="Freepik"> Freepik </a>
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com </a>
-      is licensed by
-      <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"> CC 3.0 BY </a>
-    </div>
   </div>
   );
 }
--- src/components/SeriesListItem.js
+++ src/components/SeriesListItem.js
@@ -4,15 +4,31 @@ import './styles/SeriesList.css';
 
 const SeriesListItem = ({series}) => {
 
-  const imageUrl = series.show.image.medium;
+  let imageUrl = series.show.image.medium;
+  let rating = series.show.rating.average;
+
+  if (rating == null) {
+    rating = "No rating";
+  }
+
+  if (imageUrl == null) {
+    imageUrl = "#";
+  }
 
   return (
     <li className="four wide column" key={series.show.id}>
       <div>
         <ul>
-          <li>{series.show.name}</li>
+          <li><img src={imageUrl}></img></li>
+          <li><h4 className="ui header">{series.show.name}</h4></li>
+          <li>
+            <div class="ui label">
+              <i class="star icon"></i> {rating}
+            </div>
+          </li>
+          <li>{series.show.type}</li>
+
           <li>Language: {series.show.language}</li>
-          <img src={imageUrl}></img>
         </ul>
       </div>
     </li>
