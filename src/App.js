78918f38a76199067c652839c0ed0ba72a3f39c8
inessa luerce <idluerce@thoughtworks.com>
2018-08-27 14:08:34 -0300

    Add appName to Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
+import Header from './components/Header';
 import Series from './containers/Series';
 import './App.css';
 
+
 class App extends Component {
+
   render() {
+    const appName = this.props.name;
+    console.log("app name =", appName);
+
     return (
       <div className="App">
-        <h1 className="ui header">{this.props.name} - Find your favourite series</h1>
-      <Series/>
 
+      <Header name={appName} />
+      <Series/>
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+
+  return (
+      <h1 className="ui header">{props.name} - Find your favourite series</h1>
+  )
+}
+
+export default Header;
