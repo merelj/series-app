71146d0599c0361c2509ce0c401193c517a244dd
inessa luerce <idluerce@thoughtworks.com>
2018-08-27 21:51:42 -0300

    Add jest to the project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="ui header">{this.props.name} - Find your favourite series</h1>
-      <Series/>
-
+        {/* <h1 className="ui header">{this.props.name} - Find your favourite series</h1> */}
+    {/* <Series/> */}
+      <h1>Hello world</h1>
       </div>
     );
   }
