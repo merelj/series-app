c3b9356d392233a2e35740e62fb4ad867bd28dc1
inessa luerce <idluerce@thoughtworks.com>
2018-09-07 15:49:11 -0300

    Tests h1 content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import './App.css';
-
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" data-test='component-app'>
+        <h1 className="headerOne" data-test='header-one'>Series</h1>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,41 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without crashing', () => {
-    const wrapper = shallow(<App />);
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @param {any} state - Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+    return shallow(<App {...props} />)
+}
 
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search with
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without crashing', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.length).toBe(1);
+});
+
+test('h1 should be rendered', () => {
+    const wrapper = setup();
+    const headerOne = findByTestAttr(wrapper, 'header-one');
+    expect(headerOne.length).toBe(1);
+});
+
+test('expect h1 to have "Series" text', () => {
+    const wrapper = setup();
+    const headerOne = findByTestAttr(wrapper, 'header-one');
+    expect(headerOne.text()).toContain('Series');
 });
